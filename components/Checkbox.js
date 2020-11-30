a4004753805507d07975335546201d7aeaaddd47
Marigameo <mariappangameo@gmail.com>
2020-11-30 23:30:39 +0530

    show/hide logic

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -1,11 +1,73 @@
 import React from 'react'
 import styles from '../styles/checkbox.module.css'
 export default function Checkbox (props) {
+    const { setSourceVisible, isSourceVisible, keyType, isMediumVisible, isContentVisible, isTermVisible, setInstagramVisible, setMediumVisible, setContentVisible, setTermVisible, isLinkedinVisible, isFacebookVisible, isTwitterVisible, isInstagramVisible, setLinkedinVisible, setFacebookVisible, setTwitterVisible } = props;
+    console.log(keyType)
+    const checkType = () => {
+        switch (keyType) {
+            case 'source':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setSourceVisible(!isSourceVisible)}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'medium':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setMediumVisible(!isMediumVisible)}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'content':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setContentVisible(!isContentVisible)}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'term':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setTermVisible(!isTermVisible)}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'linkedin':
+                console.log(isLinkedinVisible)
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setLinkedinVisible(!isLinkedinVisible)} checked={isLinkedinVisible}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'facebook':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setFacebookVisible(!isFacebookVisible)} checked={isFacebookVisible}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'twitter':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setTwitterVisible(!isTwitterVisible)} checked={isTwitterVisible}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+            case 'instagram':
+                return (
+                    <>
+                        <input type="checkbox" className={styles.checkbox} onChange={() => setInstagramVisible(!isInstagramVisible)} checked={isInstagramVisible}></input>
+                        <span className={styles.label}>{props.children}</span>
+                    </>
+                )
+        }
+    }
+    const checkbox = keyType ? checkType() : null;
+    console.log(checkbox)
     return (
-        <>
-            <input type="checkbox" className={styles.checkbox}></input>
-            <span className={styles.label}>{props.children}</span>
-        </>
+        <span>{checkbox}</span>
     )
 }
 
--- components/Form.js
+++ components/Form.js
@@ -4,41 +4,46 @@ import Input from './Input'
 import styles from '../styles/form.module.css'
 import ResultArea from './ResultArea'
 export default function Form () {
+    const [isSourceVisible, setSourceVisible] = useState(false)
     const [isMediumVisible, setMediumVisible] = useState(false)
     const [isContentVisible, setContentVisible] = useState(false)
     const [isTermVisible, setTermVisible] = useState(false)
     const [isInstagramVisible, setInstagramVisible] = useState(false)
+    const [isLinkedinVisible, setLinkedinVisible] = useState(true)
+    const [isTwitterVisible, setTwitterVisible] = useState(true)
+    const [isFacebookVisible, setFacebookVisible] = useState(true)
 
     return (
         <>
             <div className={styles.displayInline}>
-                <Checkbox>Source</Checkbox>
-                <Checkbox>Medium</Checkbox>
-                <Checkbox>Content</Checkbox>
-                <Checkbox>Campaign Term</Checkbox>
+                <Checkbox keyType="source" isSourceVisible={isSourceVisible} setSourceVisible={setSourceVisible}>Source</Checkbox>
+                <Checkbox keyType="medium" isMediumVisible={isMediumVisible} setMediumVisible={setMediumVisible}>Medium</Checkbox>
+                <Checkbox keyType="content" isContentVisible={isContentVisible} setContentVisible={setContentVisible}>Content</Checkbox>
+                <Checkbox keyType="term" isTermVisible={isTermVisible} setTermVisible={setTermVisible}>Campaign Term</Checkbox>
             </div>
             <div className={styles.row1}>
                 <Input>URL: </Input>
                 <Input>Campaign-name: </Input>
             </div>
-            <div className={styles.row2}>
+            <div className={styles.row2 + ' ' + styles.displayInline}>
+                <span className={`${isSourceVisible ? styles.visible : styles.hidden}`}><Input>Source: </Input></span>
                 <span className={`${isMediumVisible ? styles.visible : styles.hidden}`}><Input>Medium: </Input></span>
                 <span className={`${isContentVisible ? styles.visible : styles.hidden}`}><Input>Content: </Input></span>
                 <span className={`${isTermVisible ? styles.visible : styles.hidden}`}><Input>Term: </Input></span>
             </div>
             <div className={styles.displayInline}>
-                <Checkbox>Linkedin</Checkbox>
-                <Checkbox>Facebook</Checkbox>
-                <Checkbox>Twitter</Checkbox>
-                <Checkbox>Instagram</Checkbox>
+                <Checkbox keyType="linkedin" isLinkedinVisible={isLinkedinVisible} setLinkedinVisible={setLinkedinVisible}>Linkedin</Checkbox>
+                <Checkbox keyType="facebook" isFacebookVisible={isFacebookVisible} setFacebookVisible={setFacebookVisible}>Facebook</Checkbox>
+                <Checkbox keyType="twitter" isTwitterVisible={isTwitterVisible} setTwitterVisible={setTwitterVisible}>Twitter</Checkbox>
+                <Checkbox keyType="instagram" isInstagramVisible={isInstagramVisible} setInstagramVisible={setInstagramVisible}>Instagram</Checkbox>
             </div>
-            <div className={styles.displayInline + ' ' + styles.mgT20}>
+            <div className={styles.displayInline + ' ' + styles.mgT20 + ' ' + `${isLinkedinVisible ? styles.visible : styles.hidden}`}>
                 <ResultArea>Linkedin</ResultArea>
             </div>
-            <div className={styles.displayInline + ' ' + styles.mgT20}>
+            <div className={styles.displayInline + ' ' + styles.mgT20 + ' ' + `${isFacebookVisible ? styles.visible : styles.hidden}`}>
                 <ResultArea>Facebook</ResultArea>
             </div>
-            <div className={styles.displayInline + ' ' + styles.mgT20}>
+            <div className={styles.displayInline + ' ' + styles.mgT20 + ' ' + `${isTwitterVisible ? styles.visible : styles.hidden}`}>
                 <ResultArea>Twitter</ResultArea>
             </div>
             <div className={styles.displayInline + ' ' + styles.mgT20 + ' ' + `${isInstagramVisible ? styles.visible : styles.hidden}`}>
